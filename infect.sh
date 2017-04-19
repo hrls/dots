70c6601c2c7b39c6b843a57f75bb6dbf31e6c010
hrls <viktor.kharitonovich@gmail.com>
2017-04-19 14:22:11 +0300

    see no evil

--- infect.sh
+++ infect.sh
@@ -3,6 +3,7 @@
 ln -sf src/dots/.gitconfig  $HOME
 ln -sf src/dots/.gitignore  $HOME
 ln -sf src/dots/.vimrc      $HOME
+ln -sf src/dots/.emacs      $HOME
 ln -sf src/dots/.zshrc      $HOME
 ln -sf src/dots/.etc/       $HOME
 
