944c5fc151e977dd8153c95fa8cf4816fcb0f506
hrls <viktor.kharitonovich@gmail.com>
2017-05-03 21:59:43 +0300

    now infestor know The Source

--- .etc/sh.py
+++ .etc/sh.py
@@ -1,18 +1,19 @@
 # import sys    
 # sys.modules[__name__] = Sh(...)
 import os, os.path as path
+from functools import update_wrapper
 
 home = path.expanduser('~')
 pwd = lambda: os.getcwd()
 cd = lambda d: os.chdir(d)
 is_there = lambda f: path.islink(f) or path.isfile(f)
+shell = lambda s: os.system(s)
+
 def rm(f):
     if path.islink(f):
         os.unlink(f)
     elif path.isfile(f):
         os.remove(f)
-git_clone = lambda repo: os.system('git clone {}'.format(repo))
-shell = lambda s: os.system(s)
 
 def p(*ls):
     return path.sep.join(ls)
@@ -25,10 +26,25 @@ def rel(base, tdir):
     return path.relpath(path.expanduser(base),
                         path.expanduser(tdir))
 
-def restore_cwd(f):
-    def wrap():
+def restore_cwd(f, *args, **kwargs):
+    def wrap_cdpwd(*args, **kwargs):
         cwd = pwd()
-        f()
+        y = f(*args, **kwargs)
         cd(cwd)
-    return wrap
+        return y
+    return update_wrapper(wrap_cdpwd, f)
 
+
+git = lambda cmd: os.system('git {}'.format(cmd))
+git.clone = lambda repo: git('clone {}'.format(repo))
+git.pull = lambda: git('pull')
+# git.__class__.__getattribute__ = lambda self, cmd: self(cmd)
+
+@restore_cwd
+def github(repo):
+    repo_dir = repo.split(path.sep)[1]
+    if path.isdir(repo_dir):
+        cd(repo_dir)
+        git.pull()
+    else:
+        git.clone('https://github.com/{}'.format(repo))
--- infect.py
+++ infect.py
@@ -26,25 +26,25 @@ def link_dots():
 # home/.emacs.d/
 @restore_cwd
 def install_emacs_pkgs():
-    fp = p(home, '.emacs.d')
-    ensure_dir(fp)
-    cd(fp)
+    emacs_d = p(home, '.emacs.d')
+    ensure_dir(emacs_d)
+    cd(emacs_d)
 
-    def use_package():
-        git_clone('https://github.com/jwiegley/use-package')
+    use_package = lambda: github('jwiegley/use-package')
+    markdown_mode = lambda: github('jrblevin/markdown-mode')
 
     @restore_cwd
     def haskell_mode():
         # https://github.com/haskell/haskell-mode#installation-from-git-repository
-        git_clone('https://github.com/haskell/haskell-mode')
+        github('haskell/haskell-mode')
         cd('haskell-mode')
         shell('make EMACS=/Applications/Emacs.app/Contents/MacOS/Emacs')
 
     use_package()
+    markdown_mode()
     haskell_mode()
 
 if __name__ == '__main__':
     ensure_dir(home)
     link_dots()
-    # install_emacs_pkgs()
-
+    install_emacs_pkgs()
